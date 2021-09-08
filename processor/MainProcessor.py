cdfa09f729b010f0e83b1b9a2bef9b0386e0fbdf
winiloy <52466409+winiloy@users.noreply.github.com>
2021-09-08 11:00:47 +0600

    Add files via upload

---
+++ processor/MainProcessor.py
@@ -0,0 +1,43 @@
+from Database import Database
+from processor.TrafficProcessor import TrafficProcessor
+from processor.violation_detection import DirectionViolationDetection
+
+
+
+class MainProcessor:
+
+    def __init__(self, camera_id):
+        self.cam_id = camera_id
+        self.cam_violation_count, self.cam_location, self.cam_feed = Database.get_instance().get_cam_details(camera_id)
+
+        if camera_id == 'cam_01' or camera_id == 'cam_03' or camera_id == 'cam_05':
+            self.processor = TrafficProcessor()
+            self.processor.zone1 = (100, 150)
+            self.processor.zone2 = (450, 145)
+            self.processor.thres = 30
+
+        elif camera_id == 'cam_02':
+            self.processor = TrafficProcessor()
+            self.processor.zone1 = (100, 150)
+            self.processor.zone2 = (450, 145)
+            self.processor.thres = 6
+            self.processor.dynamic = True
+        elif camera_id == 'cam_04':
+            self.processor = DirectionViolationDetection(self.cam_feed)
+        elif camera_id == 'cam_06':
+            self.processor = DirectionViolationDetection(self.cam_feed)
+
+    def getProcessedImage(self, frame=None, cap=None):
+        if self.cam_id in ['cam_01', 'cam_02', 'cam_03', 'cam_05']:
+            dicti = self.processor.cross_violation(frame)
+
+        elif self.cam_id == 'cam_04' or 'cam_06':
+            dicti = self.processor.feedCap(frame)
+
+        return dicti
+
+    def setLight(self, color):
+        self.processor.light = color
+
+    def getLight(self):
+        return self.processor.light
